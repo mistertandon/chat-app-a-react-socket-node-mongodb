d216e1a570e7c86bfc983176e8dd477bd9a14bdc
mistretandon <enggparveshtandon@gmail.com>
2020-10-21 12:47:39 +0530

    feat: Demonstrate how get request works in Flask API

--- app.py
+++ app.py
@@ -4,13 +4,22 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+names = {
+    "tim": {
+        "age": 19,
+        "gender": "male"
+    },
+    "ronny": {
+        "age": 21,
+        "gender": "female"
+    }
+}
+
 class HelloWorld(Resource):
 
     def get(self, name, age):
-        return {
-            'name': name,
-            'age': age
-        }
+
+        return names[name]
 
 
 api.add_resource(HelloWorld, "/hello/<string:name>/<int:age>")
