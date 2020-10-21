398da366d3abca37812705d40c0e460f00e39952
mistretandon <enggparveshtandon@gmail.com>
2020-10-21 13:26:49 +0530

    feat: Develop code to parse arguments in PUT request

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {
-    "tim": {
-        "age": 19,
-        "gender": "male"
-    },
-    "ronny": {
-        "age": 21,
-        "gender": "female"
-    }
-}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type = str, help = "Name of the video")
+video_put_args.add_argument("likes", type = int, help = "Likes of the video")
+video_put_args.add_argument("views", type = int, help = "Views of the video")
 
-class HelloWorld(Resource):
+videos = {}
 
-    def get(self, name, age):
+class Video(Resource):
 
-        return names[name]
+    def get(self, video_id):
 
+        return videos[video_id]
+    
+    def put(self, video_id):
+        
+        args = video_put_args.parse_args()
+        print(args)
+        return {video_id: args}
 
-api.add_resource(HelloWorld, "/hello/<string:name>/<int:age>")
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug = True)
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import requests
 BASE_URL = "http://127.0.0.1:5000/"
 
-
-get_response = requests.get(BASE_URL + '/hello/tim/32')
+get_response = requests.put(BASE_URL + "video/32", {"likes": 15})
 print(get_response.json())
